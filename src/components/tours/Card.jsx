d5b4ed407281005a533034889af55fa34e644bc3
William <williamxtez14@gmail.com>
2021-04-05 17:56:53 -0500

    cambios trabajo

--- src/components/tours/Card.jsx
+++ src/components/tours/Card.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const Card = ({ image, title, description }) => {
-	return (
-		<div className="position-relative contain w-75 h-25 tour-border">
-			{image}
-			<div className="overlay tour-border">
-				<div className="w-75 mx-auto">
-					<h5 className="card-title">{title}</h5>
-					<p className="card-text">{description}</p>
-				</div>
-			</div>
-		</div>
-	);
+    return (
+        <div className="position-relative tour-card-container w-75 h-15 tour-border">
+            {image}
+            <div className="tour-card-overlay tour-border">
+                <div className="w-75 mx-auto">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{description}</p>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Card;
--- src/components/tours/Tours.jsx
+++ src/components/tours/Tours.jsx
@@ -1,62 +1,52 @@
-import React from 'react';
-import Card from './Card';
-import { StaticImage } from 'gatsby-plugin-image';
+import React from "react";
+import Card from "./Card";
+import { StaticImage } from "gatsby-plugin-image";
 
 const Tours = () => {
-	return (
-		<section
-			style={{
-				backgroundColor: '#85c43f',
-			}}
-			id="catalogue"
-			className="d-flex flex-column vh-100 w-100 justify-content-center align-items-center"
-		>
-			<div className="mb-4">
-				<h1>Nuestros TOURS</h1>
-			</div>
-			<div className="container h-75 d-flex flex-column justify-content-evenly align-items-center">
-				<Card
-					image={
-						<StaticImage
-							className="tour-border tour-image"
-							src={'../../images/Buff_tailed_skilebill.jpg'}
-							alt="MainImage"
-						/>
-					}
-					title={'CATÁLOGO DE AVES'}
-					description={
-						'Paraiso de orquideas y aves, un viaje que permite a los amantes de la naturaleza un memorable acercamiento a este mundo mágico.'
-					}
-				/>
-				<Card
-					image={
-						<StaticImage
-							className="tour-border tour-image"
-							src={'../../images/Buff_tailed_skilebill.jpg'}
-							alt="MainImage"
-						/>
-					}
-					title={'CATÁLOGO DE AVES'}
-					description={
-						'Paraiso de orquideas y aves, un viaje que permite a los amantes de la naturaleza un memorable acercamiento a este mundo mágico.'
-					}
-				/>
-				<Card
-					image={
-						<StaticImage
-							className="tour-border tour-image"
-							src={'../../images/Buff_tailed_skilebill.jpg'}
-							alt="MainImage"
-						/>
-					}
-					title={'CATÁLOGO DE AVES'}
-					description={
-						'Paraiso de orquideas y aves, un viaje que permite a los amantes de la naturaleza un memorable acercamiento a este mundo mágico.'
-					}
-				/>
-			</div>
-		</section>
-	);
+    return (
+        <section
+            style={{
+                backgroundColor: "#85c43f",
+                height: "200vh",
+            }}
+            id="catalogue"
+            className="d-flex flex-column w-100 justify-content-center align-items-center"
+        >
+            <div className="mb-4">
+                <h1>Nuestros TOURS</h1>
+            </div>
+            <div className="container h-75 d-flex flex-column justify-content-evenly align-items-center">
+                <Card
+                    image={<StaticImage className="tour-border tour-image" src={"../../images/tour1.jpg"} alt="MainImage" />}
+                    title={"CATÁLOGO DE AVES"}
+                    description={
+                        "Paraiso de orquideas y aves, un viaje que permite a los amantes de la naturaleza un memorable acercamiento a este mundo mágico."
+                    }
+                />
+                <Card
+                    image={<StaticImage className="tour-border tour-image" src={"../../images/tour2.jpg"} alt="MainImage" />}
+                    title={"CATÁLOGO DE AVES"}
+                    description={
+                        "Paraiso de orquideas y aves, un viaje que permite a los amantes de la naturaleza un memorable acercamiento a este mundo mágico."
+                    }
+                />
+                <Card
+                    image={<StaticImage className="tour-border tour-image" src={"../../images/tour3.jpg"} alt="MainImage" />}
+                    title={"CATÁLOGO DE AVES"}
+                    description={
+                        "Paraiso de orquideas y aves, un viaje que permite a los amantes de la naturaleza un memorable acercamiento a este mundo mágico."
+                    }
+                />
+                <Card
+                    image={<StaticImage className="tour-border tour-image" src={"../../images/tour4.jpg"} alt="MainImage" />}
+                    title={"CATÁLOGO DE AVES"}
+                    description={
+                        "Paraiso de orquideas y aves, un viaje que permite a los amantes de la naturaleza un memorable acercamiento a este mundo mágico."
+                    }
+                />
+            </div>
+        </section>
+    );
 };
 
 export default Tours;
