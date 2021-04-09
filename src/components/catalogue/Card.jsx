622ad2ea3db243f718ecf50b19c95ad2b7339efd
William <williamxtez14@gmail.com>
2021-04-09 09:52:03 -0500

    cambios locales

--- src/components/catalogue/Card.jsx
+++ src/components/catalogue/Card.jsx
@@ -1,23 +1,19 @@
-import React from 'react';
-import { Link } from 'gatsby';
+import React from "react";
+import { Link } from "gatsby";
 
 const Card = ({ image, title, description, ruta }) => {
-	return (
-		<div className="card w-35 catalogue-card text-center">
-			{image}
-			<div className="card-body">
-				<h5 className="card-title">{title}</h5>
-				<p className="card-text">{description}</p>
-				{/*  stretched-link */}
-				{/* <a href="" className="btn btn-primary">
-					Ver más
-				</a> */}
-				<Link className="btn btn-success" to={`/${ruta}`}>
-					Ver más
-				</Link>
-			</div>
-		</div>
-	);
+    return (
+        <div className="card w-35 catalogue-card text-center">
+            {image}
+            <div className="card-body h-50 d-flex align-items-center justify-content-between flex-column">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{description}</p>
+                <Link className="btn btn-success" to={`/${ruta}`}>
+                    Ver más
+                </Link>
+            </div>
+        </div>
+    );
 };
 
 export default Card;
--- src/components/tours/Card.jsx
+++ src/components/tours/Card.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const Card = ({ image, title, description }) => {
-	return (
-		<div className="position-relative m-2 tour-card-container w-75 h-15 tour-border">
-			{image}
-			<div className="tour-card-overlay tour-border">
-				<div className="w-75 mx-auto">
-					<h5 className="card-title">{title}</h5>
-					<p className="card-text">{description}</p>
-				</div>
-			</div>
-		</div>
-	);
+    return (
+        <div className="position-relative m-2 tour-card-container w-75 h-15 tour-border">
+            {image}
+            <div className="tour-card-overlay tour-border w-75">
+                <div className="w-100">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{description}</p>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Card;
