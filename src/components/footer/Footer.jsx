43fbbac2f3e1b03b73f7ce3178c7c57911ef19b0
Daniela <danielapv200@gmail.com>
2021-04-10 00:35:55 -0500

    resize

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -15,7 +15,7 @@ const NavBar = () => {
 	return (
 		<section className="section-footer text-white">
 			<div className="row">
-				<div className="col-6 text-left">
+				<div className="col-lg-6 text-left mb-5">
 					<h5 className="fw-bold pb-4">
 						CONTÁCTANOS
 					</h5>
@@ -45,7 +45,7 @@ const NavBar = () => {
 						</div>
 					</div>
 				</div>
-				<div className="col-6 text-left">
+				<div className="col-lg-6 text-left">
 					<div>
 						<h5 className="fw-bold pb-3">
 							REDES SOCIALES
@@ -71,14 +71,14 @@ const NavBar = () => {
 							/>
 						</div>
 					</div>
-					<div className="mt-4">
+					<div className="mt-5">
 						<h5 className="fw-bold pb-3">
 							UBÍCANOS
 						</h5>
 						<div>
 							<iframe 
 							src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3336.186740028314!2d-76.9748769638866!3d-6.074369526224734!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91b73a9ccd06e39b%3A0x2696e92846e6fb49!2sOrquidario+Wakanki%2C+Moyobamba!5e0!3m2!1ses-419!2spe!4v1507416439830" 
-							width="85%" height="270" frameborder="0" style={{border: '0', borderRadius:'2rem'}} allowfullscreen=""></iframe>
+							width="100%" height="270" frameborder="0" style={{border: '0', borderRadius:'2rem'}} allowfullscreen=""></iframe>
 						</div>
 					</div>
 				</div>
