c7a90fa58159f7b67aaa4ee3d693981f5931c165
William <williamxtez14@gmail.com>
2021-04-19 10:26:05 -0500

    video url https

--- src/components/promocionalvideo/PromocionalVideo.jsx
+++ src/components/promocionalvideo/PromocionalVideo.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import WaqankiVideo from "../../videos/waqanki.mp4";
+// import WaqankiVideo from "../../videos/waqanki.mp4"
 
 export default function PromocionalVideo() {
     return (
         <section style={{ backgroundColor: "#eeeeee" }}>
             <div className="video-container">
                 <video muted loop controls>
-                    <source src={WaqankiVideo} type="video/mp4" />
+                    <source src="https://waqankilodge.pe/video/waqanki.mp4" type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>
             </div>
