b77133c014d462594514b5af74b6fe3d924401bc
William <williamxtez14@gmail.com>
2021-04-19 10:29:40 -0500

    autoplay

--- src/components/promocionalvideo/PromocionalVideo.jsx
+++ src/components/promocionalvideo/PromocionalVideo.jsx
@@ -5,7 +5,7 @@ export default function PromocionalVideo() {
     return (
         <section style={{ backgroundColor: "#eeeeee" }}>
             <div className="video-container">
-                <video muted loop controls>
+                <video muted autoPlay loop controls>
                     <source src="https://waqankilodge.pe/video/waqanki.mp4" type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>
