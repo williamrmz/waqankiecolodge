3ccec188a5f77e1833aea8edbaed62eeec9c9067
William <Williamomarramirezmartinez@gmail.com>
2021-06-03 20:36:09 -0500

    locales

--- src/pages/tour.jsx
+++ src/pages/tour.jsx
@@ -1,47 +1,50 @@
-import React from "react";
-import "../styles/styles.css";
-import { graphql } from "gatsby";
-import Seo from "../components/Seo";
-import Layout from "../components/Layout";
-import HeaderTour from "../components/tour/HeaderTour";
-import Tour from "../components/tour/Tour";
-import Guide from "../components/tour/guides/GuideCard";
-import GuideModal from "../components/tour/guides/GuideModal";
+import React from 'react';
+import '../styles/styles.css';
+import { graphql } from 'gatsby';
+import Seo from '../components/Seo';
+import Layout from '../components/Layout';
+import HeaderTour from '../components/tour/HeaderTour';
+import Tour from '../components/tour/Tour';
+import Guide from '../components/tour/guides/GuideCard';
+import GuideModal from '../components/tour/guides/GuideModal';
 
 const tour = ({ location }) => {
-    if (location.state === null) return (window.location = "/");
+	if (location.state === null) {
+		return (window.location = '/');
+	} else {
+		const rute = location?.state?.rute || 'main';
+		const title = location?.state?.title || 'no title';
 
-    const { rute, title } = location.state;
+		return (
+			<div className="container-fluid p-0">
+				<Layout>
+					<Seo />
+					<HeaderTour title={title} />
+					<main>
+						<Guide />
+						<Tour title={title} rute={rute} />
+						<GuideModal />
+					</main>
+				</Layout>
+			</div>
+		);
+	}
 
-    return (
-        <div className="container-fluid p-0">
-            <Layout>
-                <Seo />
-                <HeaderTour title={title} />
-                <main>
-                    <Guide />
-                    <Tour title={title} rute={rute} />
-                    <GuideModal />
-                </main>
-            </Layout>
-        </div>
-    );
+	return <div>loading....</div>;
 };
 
 export const query = graphql`
-    query($language: String!) {
-        locales: allLocale(
-            filter: { ns: { in: ["tour"] }, language: { eq: $language } }
-        ) {
-            edges {
-                node {
-                    ns
-                    data
-                    language
-                }
-            }
-        }
-    }
+	query($language: String!) {
+		locales: allLocale(filter: { ns: { in: ["tour"] }, language: { eq: $language } }) {
+			edges {
+				node {
+					ns
+					data
+					language
+				}
+			}
+		}
+	}
 `;
 
 export default tour;
