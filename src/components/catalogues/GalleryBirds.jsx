15f03bf02343fdcd5294d6f1dcf96c5b8bad2e65
William <williamxtez14@gmail.com>
2021-06-21 11:53:45 -0500

    semi last

--- src/components/catalogues/GalleryBirds.jsx
+++ src/components/catalogues/GalleryBirds.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import { graphql, useStaticQuery } from "gatsby";
 import ImageGallery from "./ImageGallery";
 import { Link } from "gatsby";
+import { useTranslation } from "gatsby-plugin-react-i18next";
 
 const Gallery = () => {
+    const { t } = useTranslation();
     const data = useStaticQuery(graphql`
         query {
             info: allFile(filter: { sourceInstanceName: { eq: "aves" } }) {
@@ -24,7 +26,7 @@ const Gallery = () => {
         <section className="d-flex flex-column h-100 w-100 justify-content-center align-items-center birs-section">
             <div className=" w-75 mt-5">
                 <h1 className="font-weight-light text-center text-lg-left mt-5 mb-5 text-white">
-                    Galería de Aves
+                    {t("catalogues-titulo-aves")}
                 </h1>
 
                 <div className="w-100 d-flex justify-content-center align-items-center">
@@ -33,7 +35,7 @@ const Gallery = () => {
                         target="_blank"
                         className="btn btn-warning btn-landing "
                     >
-                        Visita EBirds Para Registrar Tus Aves Vistos
+                        {t("boton-ebirds")}
                     </Link>
                 </div>
                 {/* <hr className="mt-2 mb-5" /> */}
--- src/components/catalogues/GalleryOrquides.jsx
+++ src/components/catalogues/GalleryOrquides.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { graphql, useStaticQuery } from "gatsby";
 import ImageGallery from "./ImageGallery";
+import { useTranslation } from "gatsby-plugin-react-i18next";
 
 const Gallery = () => {
+    const { t } = useTranslation();
     const data = useStaticQuery(graphql`
         query {
             info: allFile(filter: { sourceInstanceName: { eq: "orquideas" } }) {
@@ -23,7 +25,7 @@ const Gallery = () => {
         <section className="d-flex flex-column h-100 w-100 justify-content-center align-items-center birs-section">
             <div className=" w-75 mt-5">
                 <h1 className="font-weight-light text-center text-lg-left mt-5 mb-5 text-white">
-                    Galería de Orquídeas
+                    {t("catalogues-titulo-orquides")}
                 </h1>
 
                 {/* <hr className="mt-2 mb-5" /> */}
